c64bb0c4be7bf3ce9c04c3b4056ab8440e73e5d6
toscy <123191534+Lajous-Harold@users.noreply.github.com>
2025-10-14 16:11:45 +0200

    Push Etape 1

--- todolist-cli/src/todolist/utils/validators.py
+++
@@ -1,9 +0,0 @@
-def validate_title(title):
-    if not title or len(title) < 3:
-        raise ValueError("Title must be at least 3 characters long.")
-    return title
-
-def validate_description(description):
-    if description and len(description) > 200:
-        raise ValueError("Description must be 200 characters or less.")
-    return description
